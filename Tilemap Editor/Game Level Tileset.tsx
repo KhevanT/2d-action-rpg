52ec306ed0a3d7610df4425c69fac9bbfd8221da
Khevan Thanki <Khevanyt@gmail.com>
2023-11-15 22:55:18 +0530

    Added sword and enemy sprites

---
+++ Tilemap Editor/Game Level Tileset.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Zelda Clone" tilewidth="256" tileheight="176" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../Game Files/Art Assets/Tileset/Floor Tile 1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../Game Files/Art Assets/Tileset/Floor Tile 2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../Game Files/Art Assets/Tileset/Floor Tile 3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="176" source="../Game Files/Art Assets/Tileset/Room Exterior.png"/>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
